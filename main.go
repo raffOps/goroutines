749ee71086a0528675aa0fe000f50749749e040f
Rafael <rjribeiro@inf.ufrgs.br>
2024-03-06 17:53:14 -0300

    initial commit
    initial commit

--- main.go
+++ main.go
@@ -6,22 +6,28 @@ import (
 	"time"
 )
 
-func numeros(channel chan int, wg *sync.WaitGroup) {
+func printNumbers(channel chan int, wg *sync.WaitGroup) {
+	defer close(channel)
 	defer wg.Done()
-	for i := 0; i < 4; i++ {
+
+	for i := 0; i < 12; i++ {
 		fmt.Printf("%d ", i)
-		//time.Sleep(time.Millisecond * 150)
+		time.Sleep(time.Millisecond * 150)
 		channel <- i
 	}
-	print("")
 }
 
-func letras(channel chan rune, numberChan chan int, wg *sync.WaitGroup) {
+func printLetters(channel chan rune, numberChan chan int, wg *sync.WaitGroup) {
 	defer wg.Done()
 	for l := 'a'; l < 'n'; l++ {
 		// the letters will be printed only after the 6
-		for lock := 0; lock < 6 && l == 'a'; {
-			lock = <-numberChan
+		var number int
+		var ok bool
+		for l == 'a' && number < 6 {
+			number, ok = <-numberChan
+			if !ok {
+				break
+			}
 		}
 		fmt.Printf("%c ", l)
 		time.Sleep(time.Millisecond * 230)
@@ -29,20 +35,16 @@ func letras(channel chan rune, numberChan chan int, wg *sync.WaitGroup) {
 	}
 }
 
-func main1() {
+func main() {
 
 	var wg sync.WaitGroup
 	wg.Add(2)
 
 	numberChan := make(chan int, 100)
 	letterChan := make(chan rune, 100)
-	go numeros(numberChan, &wg)
-	go letras(letterChan, numberChan, &wg)
+	go printNumbers(numberChan, &wg)
+	go printLetters(letterChan, numberChan, &wg)
 
 	wg.Wait()
-	fmt.Printf("Fim")
-}
-
-func main() {
-	main1()
+	fmt.Printf("End")
 }
